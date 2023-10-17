c3c14116727866309e0834e27d4ed7266c9d0f05
Martin Wimpress <martin@wimpress.org>
2023-10-17 19:01:27 +0100

    refactor: correct includes

--- src/plugin-main.c
+++ src/plugin-main.c
@@ -18,9 +18,9 @@ You should have received a copy of the GNU General Public License along
 with this program. If not, see <https://www.gnu.org/licenses/>
 */
 
-#include <graphics/image-file.h>
 #include <obs-module.h>
-#include <plugin-support.h>
+#include <graphics/image-file.h>
+#include "plugin-support.h"
 
 #define FILTER_NAME "dvd-filter"
 #define SOURCE_NAME "dvd-image"
